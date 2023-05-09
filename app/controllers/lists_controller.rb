9ea951e86e966b0b1293087e3511bf5e44fd5fbb
keti kaleja <katykaleja@gmail.com>
2023-05-09 16:09:27 +0100

    Adding seed file

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,15 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
 end
